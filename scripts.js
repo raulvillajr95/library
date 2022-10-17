4325a8443b51330f7b4bb5f38d315f4581fce514
Raul <raulvillajr95@gmail.com>
2022-10-17 16:02:29 -0700

    Complete project 'Library'

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 let myLibrary = [];
 
+// Book constructor
 function Book(author, title, pages, read) {
   this.author = author;
   this.title = title;
@@ -49,40 +50,84 @@ function libraryLoop() {
     cell4.textContent = `${myLibrary[i].read}`
     row.appendChild(cell4)
 
+    // Add remove button to table
     let cell5 = document.createElement('td')
     let removeButton = document.createElement('button')
-    removeButton.textContent = 'Remove'
+    removeButton.textContent = 'REMOVE'
     removeButton.setAttribute('class','remove-button')
     removeButton.setAttribute('data-index-number', `${i}`)
     row.setAttribute('data-index-number', `${i}`)
     cell5.appendChild(removeButton)
     row.appendChild(cell5)
 
+    // Add read button
+    let cell6 = document.createElement('td')
+    let readButton = document.createElement('button')
+    readButton.textContent = `${myLibrary[i].read.toString().toUpperCase()}`
+    readButton.setAttribute('class','read-button')
+    readButton.setAttribute('data-index-number', `${i}`)
+    cell6.appendChild(readButton)
+    row.appendChild(cell6)
+
     libraryElement.appendChild(row)
   }
 }
 
 window.addEventListener('click', function(e) {
+
+  // Remove books
   if (e.composedPath()[0].classList[0] === 'remove-button') {
     let indexNum = Number(e.path[0].dataset.indexNumber)
     myLibrary.splice(indexNum,1)
     libraryLoop()
   }
+
+  // Remove books
+  if (e.composedPath()[0].classList[0] === 'read-button') {
+    let indexNum = Number(e.path[0].dataset.indexNumber)
+
+    if (myLibrary[indexNum].read === false) {
+      myLibrary[indexNum].read = true
+    } else if (myLibrary[indexNum].read === true) {
+      myLibrary[indexNum].read = false
+    }
+
+    console.log(myLibrary[indexNum].read)
+    libraryLoop()
+    console.log('read button')
+  }
 })
 
+// Add book to library
 let submitElement = document.querySelector('#submit')
 submitElement.addEventListener('click', function() {
-  addBookToLibrary()
-  libraryLoop()
+
+  let authorElement = document.querySelector('#author')
+  let titleElement = document.querySelector('#title')
+  let pagesElement = document.querySelector('#pages')
+  let readElement = document.querySelector('#read')
+
+  if (authorElement.value !== "" &&
+  titleElement.value !== "" &&
+  pagesElement.value !== "") {
+    addBookToLibrary()
+    libraryLoop()
+  }
+
+  authorElement.value = ""
+  titleElement.value = ""
+  pagesElement.value = ""
+  readElement.checked = false
 })
 
+// Toggle 'NEW BOOK' button
 let newBookElement = document.querySelector('.new-book')
 let formElement = document.querySelector('.main-form')
 let toggle = false
 newBookElement.addEventListener('click', function() {
   if (!toggle) {
     submitElement.style.display = "block"
-    formElement.style.display = "block"
+    formElement.style.display = "grid"
     newBookElement.textContent = "CLOSE"
     toggle = true
   } else if (toggle) {
@@ -95,13 +140,7 @@ newBookElement.addEventListener('click', function() {
 
 /*
 ideas:
-
-later:
--clean up the inputs, make everything readable
--css, it's gotta look good
-  ima go for the dashboard look
--after submitting, text clears
--don't allow empty boxes
+-pointer cursor on remove and read buttons
 
 Instructions:
 1. Done
@@ -109,6 +148,5 @@ Instructions:
 3. Done
 4. Done
 5. Done
-6. add button on display to change read status
-    create function that toggles 'read' on Book
+6. Done
 */
